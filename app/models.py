b675e9d783b98b6511fd24eae22b936f95a0c416
Sage Abdullah <sage.abdullah@torchbox.com>
2023-09-20 14:59:47 +0100

    Create Book model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    class Meta:
+        index_together = ["name", "author"]

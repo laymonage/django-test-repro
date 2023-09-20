7b9016f31e341a0f0548330f9da72e588c6bee42
Sage Abdullah <sage.abdullah@torchbox.com>
2023-09-20 15:00:01 +0100

    Add test for creating Book instance

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from app.models import Book
+
 # Create your tests here.
+
+class TestBook(TestCase):
+    def test_create_book(self):
+        book = Book.objects.create(name="Hello", author="World")
+        self.assertCountEqual(Book.objects.all(), [book])
